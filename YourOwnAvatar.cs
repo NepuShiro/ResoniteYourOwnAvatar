d0e30d5ac5575adf70a92ac41977e62ea9b28a79
NepuShiro <nepushiro@nepu.men>
2026-03-27 18:55:48 -0500

    Add comments, and attempt to fix race condition

--- YourOwnAvatar.cs
+++ YourOwnAvatar.cs
@@ -37,6 +37,7 @@ public class YourOwnAvatar : ResoniteMod
         {
             if (!config.GetValue(ENABLED)) return true;
 
+            // We completely skip this method, to never create a key, and to not set user.AvatarAccessKey
             __result = Task.CompletedTask;
             return false;
         }
@@ -44,28 +45,37 @@ public class YourOwnAvatar : ResoniteMod
         [HarmonyPostfix, HarmonyPatch(typeof(UserRoot), "OnStart")]
         public static void UserRootPostfix(UserRoot __instance)
         {
-            if (__instance.ActiveUser != __instance.World.LocalUser) return;
             if (!config.GetValue(ENABLED)) return;
+            // If we're not the active user of the userroot don't run.
+            if (__instance.ActiveUser != __instance.World.LocalUser) return;
 
+            // if the world is userspace or if we're the host don't run.
             World world = __instance.World;
             if (world.IsUserspace() || world.IsAuthority) return;
 
+            // Check if the CommonAvatarBuilder Exists, and if You're allowed to load Cloud Avatars.
             CommonAvatarBuilder builder = world.RootSlot.GetComponentInChildren<CommonAvatarBuilder>();
-            if (builder == null) return;
-
-            if (!builder.LoadCloudAvatars) return;
-            __instance.Slot.RunInUpdates(3, () =>
+            if (builder == null || !builder.LoadCloudAvatars) return;
+            
+            // Maybe switch this between RunInUpdates/RunInSeconds to stop edge cases
+            __instance.Slot.RunInSeconds(3, () =>
             {
                 AvatarObjectSlot objSlot = __instance.Slot.GetComponent<AvatarObjectSlot>();
                 AvatarManager manager = __instance.Slot.GetComponent<AvatarManager>();
+
+                // Try to find if the Avatar we currently have equipped is one of the CustomAvatarTemplates
                 CommonAvatarBuilder.AvatarTemplate template = null;
                 if (builder.CustomAvatarTemplates.Count > 0 && objSlot.Equipped.Target is AvatarRoot comp)
                 {
+                    // Maybe check if any of the templates block the cloud avatar for safety?
                     template = builder.CustomAvatarTemplates.FirstOrDefault(x => x.TemplateRoot.Name == comp.Slot.Name) ?? builder.CustomAvatarTemplates.FirstOrDefault(x => x.TemplateRoot.Name == comp.Slot.GetObjectRoot().Name);
                 }
 
+                // if you're in a template avatar, check if it blocks the cloud avatar
+                // or if you're defaulted spectator. Don't load the cloud avatar
                 if (template != null && template.BlockCloudAvatar.Value || __instance.LocalUser.DefaultSpectator) return;
 
+                // This is basically a copy of what the host does to spawn the avatar - CommonAvatarBuilder.SpawnCloudAvatar() ~ L54
                 Uri favoriteAvatar = world.Engine.Cloud.Profile.GetCurrentFavorite(FavoriteEntity.Avatar);
                 if (favoriteAvatar != null)
                 {
